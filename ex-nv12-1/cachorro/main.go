30cf3e47914c3d9a31a26920ef4cd7871f16a473
danilomacb <danilo.bakun@gmail.com>
2020-10-17 17:11:41 -0300

    exercicio nv12 1

---
+++ ex-nv12-1/cachorro/main.go
@@ -0,0 +1,7 @@
+// Pacote que converte idade humana para idade de cachorro
+package cachorro
+
+// Recebe um int e o multiplica por 7
+func Idade(n int) int {
+	return (n * 7)
+}
---
+++ ex-nv12-1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"aprenda-go/ex-nv12-1/cachorro"
+)
+
+func main() {
+	a := cachorro.Idade(3)
+	fmt.Println(a)
+}
